b3dd9946a5e2b46d08e74017fe7f4873db2f1ef6
vikthour01 <vikthour01@gmail.com>
2021-10-27 23:19:18 -1200

    Worked on the search-bar using filter

--- search.js
+++ search.js
@@ -1,31 +1,48 @@
 const characterList = document.querySelector("#charactersList");
+const searchBar = document.querySelector("#searchBar");
+let hpCharacters = [];
 
-async function loadCharacters(){
-    try{
-        const res = await fetch('https://hp-api.herokuapp.com/api/characters')
-        let hpCharacters = await res.json()
-        displayCharacters(hpCharacters)
-        // console.log(hpCharacters)
-    } catch(error){
-        console.error(error)
-    }
+searchBar.addEventListener("keyup", (e) => {
+//   const searchStringValue = e.target.value;
+//   const searchString =
+//     searchStringValue.charAt(0).toUpperCase() + searchStringValue.slice(1);
+const searchString = e.target.value.toLowerCase()
+  console.log(searchString);
+  const filteredCharacters = hpCharacters.filter((character) => {
+    return (
+      character.name.toLowerCase().includes(searchString) ||
+      character.house.toLowerCase().includes(searchString)
+    );
+  });
+  // console.log(filteredCharacters)
+  displayCharacters(filteredCharacters);
+});
+
+async function loadCharacters() {
+  try {
+    const res = await fetch("https://hp-api.herokuapp.com/api/characters");
+    hpCharacters = await res.json();
+    displayCharacters(hpCharacters);
+    // console.log(hpCharacters)
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function displayCharacters(characters){
-    const htmlString = characters.map((character) =>{
-         return `
+function displayCharacters(characters) {
+  const htmlString = characters
+    .map((character) => {
+      return `
         <li class="character">
             <h2>${character.name}</h2>
             <p>House: ${character.house}</p>
             <img src="${character.image}"></img>
         </li>
-    `
-        
+    `;
     })
-    .join('')
-    console.log(htmlString)
-    characterList.innerHTML = htmlString
+    .join("");
+  // console.log(htmlString)
+  characterList.innerHTML = htmlString;
 }
 
-
-loadCharacters()
+loadCharacters();
